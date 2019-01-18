04e4747704b654f754007834220fa8f3cc411d9f
Wendy Lin <wendy840123@gmail.com>
2019-01-17 17:17:09 -0800

    create a player that can move around

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame as pg
+import sys
+import SpriteTools as st
+from settings import *
+from Player import Player as ply
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+
+    def new_game(self):
+        # initialize variables to setup a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player = ply.Player(self, 10, 10) # player's initial location
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
+g = Game()
+
+while True:
+    g.new_game()
+    g.run()
+
+
